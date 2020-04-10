685e2de950d4ca711ec94425700343a63c338d84
squelsh84 <mrmjwalsh@gmail.com>
2020-04-10 17:07:43 +0100

    Removed unused code.

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,5 @@
-/**
-* Template Name: Rapid - v2.0.0
-* Template URL: https://bootstrapmade.com/rapid-multipurpose-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
+
 $(function () {
   $(window).on('scroll', function () {
     if ($(window).scrollTop() > 10) {
